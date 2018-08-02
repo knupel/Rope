4494fd171c46bec270c331ecb5d570d93568082e
Stan Le Punk <stanislasiii@gmail.com>
2018-08-03 00:42:58 +0200

    extends bVec2

--- src/rope/vector/bVec2.java
+++ src/rope/vector/bVec2.java
@@ -1,16 +1,19 @@
 package rope.vector;
 
-public class bVec2 {
+public class bVec2 extends bVec {
 	
 	public bVec2(boolean x,boolean y) {
-		
+		super(2);
 	}
 	
 	public bVec2(boolean arg) {
+		super(2);
 		
 	}
 	
 	public bVec2(bVec b) {
+		super(2);
+		
 		
 	}
 
