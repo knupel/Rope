07b69fc74b8447eeddf4cd954a6cf9354eaec4ff
Stan Le Punk <stanislasiii@gmail.com>
2018-08-02 16:23:49 +0200

    save step by step

--- src/rope/vector/Vec3.java
+++ src/rope/vector/Vec3.java
@@ -1,9 +1,105 @@
 package rope.vector;
 
 public class Vec3 extends Vec {
+	// CONSTRUCTOR
   public Vec3() {
   	super(3);
-  	//set(0,0,0);
+  	set(0,0,0);
+  }
+  
+  public Vec3(float v) {
+  	super(3);
+  	set(v,v,v);
+  }
+	
+	public Vec3(float x, float y, float z) {
+    super(3) ;
+    set(x,y,z);
+  }
+	
+	
+	
+	// SET
+  public Vec3 set(float v) {
+  	set(v,v,v);
+  	return this;
+  }
+  
+  
+  public Vec3 set(Vec v) {
+    if(v == null) {
+      set(0,0,0);
+      return this ;
+    } else if(v instanceof Vec5 || v instanceof Vec6) {
+      set(v.a,v.b,v.c);
+      return this ;
+    } else {
+      set(v.x,v.y,v.z);
+      return this ;
+    }
+  }
+  
+  public Vec3 set(iVec v) {
+    if(v == null) {
+      set(0,0,0);
+      return this ;
+    } else if(v instanceof iVec5 || v instanceof iVec6) {
+      set(v.a,v.b,v.c);
+      return this ;
+    } else {
+      set(v.x,v.y,v.z);
+      return this ;
+    }
+  }
+  
+	public Vec3 set_x(float x) {
+		return set(x,this.y,this.z);
+	}
+
+	public Vec3 set_y(float y) {
+   return set(this.x,y,this.z);
+	}
+
+	public Vec3 set_z(float z) {
+   return set(this.x,this.y,z);
+	}
+
+	// rgb
+	public Vec3 set_r(float x) {
+   return set(x,this.y,this.z);
+	}
+
+	public Vec3 set_g(float y) {
+   return set(this.x,y,this.z);
+	}
+
+	public Vec3 set_b(float z) {
+   return set(this.x,this.y,z);
+	}
+
+	// stp
+	public Vec3 set_s(float x) {
+   return set(x,this.y,this.z);
+	}
+
+	public Vec3 set_t(float y) {
+   return set(this.x,y,this.z);
+	}
+
+	public Vec3 set_p(float z) {
+   return set(this.x,this.y,z);
+	}
+ 
+  public Vec3 set(float x, float y, float z) {
+  	this.x = this.r = this.s = x;
+  	this.y = this.g = this.t = y;
+  	this.z = this.b = this.p = z;
+  	return this;
   }
 
+  
+  public Vec3 set(float[] source) {
+    set(source[0],source[1],source[2]);
+    return this ;
+  }
 }
