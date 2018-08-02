0cc7a80279dd08cd9a8a38536a8699a6a9d14368
Stan Le Punk <stanislasiii@gmail.com>
2018-08-02 17:47:14 +0200

    Vec5 slowly

--- src/rope/vector/Vec5.java
+++ src/rope/vector/Vec5.java
@@ -3,6 +3,16 @@ package rope.vector;
 public class Vec5 extends Vec {
   public Vec5() {
   	super(5);
-  	//set(0,0,0,0,0);
+  	set(0,0,0,0,0);
+  }
+  
+  
+  public Vec5 set(float a,float b, float c, float d, float e) {
+  	this.a = a;
+  	this.b = b;
+  	this.c = c;
+  	this.d = d;
+  	this.e = e;
+  	return this;
   }
 }
