3305f18788e3bcf90d51b11b0dd7151b29ea6a91
Stan Le Punk <stanislasiii@gmail.com>
2018-08-03 00:25:48 +0200

    add get_array

--- src/rope/vector/bVec.java
+++ src/rope/vector/bVec.java
@@ -13,5 +13,25 @@ public abstract class bVec extends BigBangRope {
 	public int get_num() {
 		return this.num;
 	}
+	
+	
+	public boolean [] get_array() {
+    if(num == 2) {
+      boolean array [] = {x,y};
+      return array;
+    } else if(num == 3) {
+      boolean array [] = {x,y,z};
+      return array;
+    } else if(num == 4) {
+      boolean array [] = {x,y,z,w};
+      return array;
+    } else if(num == 5) {
+      boolean array [] = {a,b,c,d,e,f};
+      return array;
+    } else if(num == 6) {
+      boolean array [] = {a,b,c,d,e,f};
+      return array;
+    } else return null ;
+  }
 
 }
