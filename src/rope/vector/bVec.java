30cb2e666dd4f6bc9d5eb15fe33a67512aeedca6
Stan Le Punk <stanislasiii@gmail.com>
2018-08-03 00:24:24 +0200

    step by step

--- src/rope/vector/bVec.java
+++ src/rope/vector/bVec.java
@@ -3,9 +3,15 @@ package rope.vector;
 import rope.core.BigBangRope;
 
 public abstract class bVec extends BigBangRope {
+	private int num ;
+	public boolean x,y,z,w;
+	public boolean a,b,c,d,e,f;
+	public bVec(int num) {
+		this.num = num;
+	}
 	
-	public bVec() {
-		
+	public int get_num() {
+		return this.num;
 	}
 
 }
